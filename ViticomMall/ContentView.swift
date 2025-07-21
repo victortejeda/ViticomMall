3558be3ac9e7509b9b82fdc7453ceba3c6060579
victortejeda <victorfernandotejeda@gmail.com>
2025-07-20 21:32:16 -0400

    Initial Commit

---
+++ ViticomMall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViticomMall
+//
+//  Created by Victor Tejeda on 20/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViticomMall/ViticomMallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViticomMallApp.swift
+//  ViticomMall
+//
+//  Created by Victor Tejeda on 20/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ViticomMallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
